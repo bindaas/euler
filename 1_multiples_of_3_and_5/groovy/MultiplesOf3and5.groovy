abad540acb9050f6976cc077f7274f8624636be5
bindaas <rajiv.narula@gmail.com>
2014-10-17 16:07:16 -0400

    Problem 1- Multiple of 3 and 5

--- 1_multiples_of_3_and_5/groovy/MultiplesOf3and5.groovy
+++ 1_multiples_of_3_and_5/groovy/MultiplesOf3and5.groovy
@@ -1,8 +1,21 @@
-public class MultiplesOf3and5{
+Integer max = new Integer(args[0])
 
-
-	public static void main (){
-
-	}
-
+def sum = 0 ;
+for ( i in 3..max-1 ) {
+    boolean isMultiple = false
+    if ((i%3) == 0){
+    	println 'Multiple of 3 :'+i
+    	isMultiple = true
+    }
+    if ((i%5) == 0){
+    	println 'Multiple of 5 :'+i
+    	isMultiple = true
+    }
+    if (isMultiple){
+    	sum += i
+    }
+    
+    
 }
+
+println sum
